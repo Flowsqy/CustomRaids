0d0e6bfa50a7f229898fb4fc05f904f478b0907f
Flowsqy <Flo504.Dev@gmail.com>
2021-11-14 13:41:12 +0100

    Init project

---
+++ src/main/java/fr/flowsqy/customraids/CustomRaidsPlugin.java
@@ -0,0 +1,12 @@
+package fr.flowsqy.customraids;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomRaidsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
